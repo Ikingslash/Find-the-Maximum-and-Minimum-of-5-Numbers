e9ad6f64fca2f73921544c00376c8ba136ad87d5
Ayodele David <ayodeled513@gmail.com>
2026-02-19 17:36:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// This is a Javascript file that contains functions to calculate the average, maximum, and minimum of five numbers.
+function maximum(a, b, c, d, e) {
+  let result = Math.max(a, b, c, d, e);
+  return result;
+}
+console.log(maximum(90, 65, 32, 40, 78));
+
+function minimum(a, b, c, d, e) {
+  let result = Math.min(a, b, c, d, e);
+  return result;
+}
+console.log(minimum(90, 65, 32, 40, 78));
